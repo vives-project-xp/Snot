30aab9238a53db8cd3b7c97d6f92209ddd0db00a
Szymon Dizewski <szymon.dizewski1@gmail.com>
2022-10-20 12:35:24 +0200

    del

---
+++ mbed/headers/EEPROMDriver.cpp
@@ -0,0 +1,10 @@
+
+#include "mbed.h"
+#include "EEPROMDriver.h"
+#include "m24sr_driver.h"
+
+mbed::nfc::NFCEEPROMDriver& get_eeprom_driver(events::EventQueue&)
+{
+    static mbed::nfc::vendor::ST::M24srDriver eeprom_driver;
+    return eeprom_driver;
+}
