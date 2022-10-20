a21270dfac0e8afa85acc89db28f9d32ff187da1
Szymon Dizewski <szymon.dizewski1@gmail.com>
2022-10-20 09:54:06 +0200

    nfc branch init

--- mbed/src/main.cpp
+++ mbed/src/main.cpp
@@ -1,39 +1,93 @@
+/*
+ * Copyright (c) 2006-2020 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include "mbed.h"
-#include "Servo.h"
- 
- 
-PwmOut motorFwd(D5);
-PwmOut motorRev(D6); 
-Servo myservo(D9);
-InterruptIn user_button(BUTTON1);
+#include "nfc/ndef/MessageBuilder.h"
+#include "nfc/ndef/common/URI.h"
+#include "nfc/ndef/common/Text.h"
+#include "nfc/ndef/common/util.h"
+#include "nfc/ndef/common/SimpleMessageParser.h"
 
-void tighten() {
-    for(float p = 0.2; p<0.55; p += 0.1) {
-        myservo.write(p);
-        wait_us(200000);
-    }
-}
+using mbed::nfc::ndef::Record;
+using mbed::nfc::ndef::RecordType;
+using mbed::nfc::ndef::RecordID;
+using mbed::nfc::ndef::MessageParser;
+using mbed::nfc::ndef::common::Text;
+using mbed::nfc::ndef::common::URI;
+using mbed::nfc::ndef::common::Mime;
+using mbed::nfc::ndef::MessageBuilder;
+using mbed::nfc::ndef::common::span_from_cstr;
+using mbed::nfc::ndef::common::SimpleMessageParser;
 
-void loss() {
-    myservo.write(0.2);
-}
+int main()
+{
+    uint8_t message_data[512];
+    const Span<uint8_t, 512> buffer(message_data, sizeof(message_data));
 
-void motorOn() {
-    printf("DRAAI!\n");
-    motorFwd.write(1);
-    motorRev.write(0);
-}
+    MessageBuilder builder(buffer);
 
-void motorOff() {
-    printf("STOP!\n");
-    motorFwd.write(0);
-    motorRev.write(0);
-}
+    Text text(Text::UTF8, span_from_cstr("en-US"), span_from_cstr("Mbed website"));
+    URI uri(URI::HTTPS_WWW, span_from_cstr("mbed.com"));
 
-int main() {
-    // user_button.fall(motorOn);
-    // user_button.rise(motorOff);
+    uri.append_as_record(builder);
+    text.append_as_record(builder, /* last record */ true);
 
-    user_button.rise(loss);
-    user_button.fall(tighten);
+    SimpleMessageParser parser;
+    struct : SimpleMessageParser::Delegate {
+        virtual void on_parsing_started()
+        {
+            printf("parsing started\r\n");
+        }
+
+        virtual void on_text_parsed(const Text &text, const RecordID &)
+        {
+            printf("Text record parsed.\r\n");
+            printf(
+                "\tlanguage: %.*s\r\n",
+                text.get_language_code().size(), text.get_language_code().data()
+            );
+            printf("\ttext: %.*s\r\n",  text.get_text().size(), text.get_text().data());
+        }
+
+        virtual void on_uri_parsed(const URI &uri, const RecordID &)
+        {
+            printf("URI parsed.\r\n");
+            printf("\tid: %d\r\n", uri.get_id());
+            printf("\tvalue: %.*s\r\n",  uri.get_uri_field().size(), uri.get_uri_field().data());
+        }
+
+        virtual void on_mime_parsed(const Mime &mime, const RecordID &)
+        {
+            printf("Mime object parsed.\r\n");
+            printf("\ttype: %.*s\r\n", mime.get_mime_type().size(), mime.get_mime_type().data());
+            printf("\tcontent size: %d\r\n", mime.get_mime_content().size());
+        }
+
+        virtual void on_unknown_record_parsed(const Record &record)
+        {
+            printf("Unknown record parsed.\r\n");
+            printf(
+                "\ttype: %d, type_value: %.*s\r\n",
+                record.type.tnf, record.type.value.size(), record.type.value.data()
+            );
+            printf(
+                "\tpayload size: %d, payload: %.*s\r\n",
+                record.payload.size(), record.payload.size(), record.payload.data()
+            );
+        }
+
+        virtual void on_parsing_terminated()
+        {
+            printf("parsing terminated\r\n");
+        }
+
+        virtual void on_parsing_error(MessageParser::error_t error)
+        {
+            printf("Parsing error: %d\r\n", error);
+        }
+    } delegate;
+
+    parser.set_delegate(&delegate);
+    parser.parse(buffer);
 }
