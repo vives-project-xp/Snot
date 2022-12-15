f57afcf3cd3f54c62008438536af9e4f636db874
Szymon Dizewski <szymon.dizewski1@gmail.com>
2022-12-15 13:06:59 +0100

    fix RGB value used only rValue

--- mbed/src/helperes/Led.cpp
+++ mbed/src/helperes/Led.cpp
@@ -9,28 +9,28 @@ namespace Helpers{
   }
 
   void Led::red(void){
-    _red->write(1);
-    _green->write(0);
-    _blue->write(0);
+    _red->write(0);
+    _green->write(1);
+    _blue->write(1);
   }
 
   void Led::green(void){
-    _red->write(0);
-    _green->write(1);
-    _blue->write(0);
+    _red->write(1);
+    _green->write(0);
+    _blue->write(1);
   }
 
   void Led::blue(void){
-    _red->write(0);
-    _green->write(0);
-    _blue->write(1);
+    _red->write(1);
+    _green->write(1);
+    _blue->write(0);
 
   }
 
   void Led::clear(void){
-    _red->write(0);
-    _green->write(0);
-    _blue->write(0);
+    _red->write(1);
+    _green->write(1);
+    _blue->write(1);
   }
 
   void Led::RGB(int red, int green, int blue){
@@ -38,9 +38,9 @@ namespace Helpers{
     if(green > 255 || green < 0) return;
     if(blue > 255 || blue < 0) return;
 
-    int rValue = red/255;
-    int gValue = green/255;
-    int bValue = blue/255;
+    int rValue = (255 - red) /255;
+    int gValue = (255 - green)/255;
+    int bValue = (255 - blue) /255;
 
     _red->write(rValue);
     _green->write(gValue);
